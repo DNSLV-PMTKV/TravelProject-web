82c95dc1fc3e57a62f79addde5d4e4621a7b1f26
DNSLV-PMTKV <pometkov.d@protonmail.com>
2020-12-10 20:44:34 +0200

    idk what im doing (:

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
+import { ThemeProvider } from '@material-ui/core';
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import { theme } from './theme';
+import { Layout } from './containers/Layout/Layout';
 
 const LoginPage = React.lazy(() => import('./pages/login/Login'));
 
 const App: React.FC = () => {
     return (
-        <Suspense fallback={'loading..'}>
-            <Router>
-                <Switch>
-                    <Route path='/' component={LoginPage} />
-                    {/* <Route path='/register' component={RegisterPage} />
+        <ThemeProvider theme={theme}>
+            <Layout>
+                <Suspense fallback={'loading..'}>
+                    <Router>
+                        <Switch>
+                            <Route path='/' component={LoginPage} />
+                            {/* <Route path='/register' component={RegisterPage} />
                     <Route path='/login' component={LoginPage} /> */}
-                    <Redirect to='/error' />
-                </Switch>
-            </Router>
-        </Suspense>
+                            <Redirect to='/error' />
+                        </Switch>
+                    </Router>
+                </Suspense>
+            </Layout>
+        </ThemeProvider>
     );
 };
 
